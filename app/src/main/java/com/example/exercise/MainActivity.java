da89da69557fb203dad102628e30adcbfa55a0ea
leezu2020 <ejh7406@naver.com>
2020-12-21 20:57:42 +0900

    add a web crawler & edit to how to update the widget

--- app/src/main/java/com/example/exercise/MainActivity.java
+++ app/src/main/java/com/example/exercise/MainActivity.java
@@ -1,37 +1,43 @@
 package com.example.exercise;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.prefs.PreferenceChangeEvent;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int CODE = 1;
+    private TextView main_txt;
     private Button srchbtn1;
     private Button srchbtn2;
     private Button srchbtn3;
     private Button srchbtn4;
-    private Button update_widget;
     private static String srchEngine1 = "http:/m.search.naver.com/search.naver?query=";
     private static String srchEngine2 = "http:/m.search.naver.com/search.naver?query=";
     private static String srchEngine3 = "http:/m.search.naver.com/search.naver?query=";
     private static String srchEngine4 = "http:/m.search.naver.com/search.naver?query=";
 
     private final long FINISH_INTERVAL_TIME = 1500;
-    private  long backPressedTime = 0;
+    private long backPressedTime = 0;
+
+    // 0은 국내 확진자 1은 해외 확진자
+    private int[] infected = new int[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         srchbtn2 = findViewById(R.id.searchBtn2);
         srchbtn3 = findViewById(R.id.searchBtn3);
         srchbtn4 = findViewById(R.id.searchBtn4);
-        update_widget = findViewById(R.id.update_widget);
 
         SharedPreferences pref = getSharedPreferences("pref",0);
         srchbtn1.setText(pref.getString("b1","기본"));
@@ -53,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         srchEngine3 = pref.getString("e3","http:/m.search.naver.com/search.naver?query=");
         srchbtn4.setText(pref.getString("b4","입니다."));
         srchEngine4 = pref.getString("e4","http:/m.search.naver.com/search.naver?query=");
+
 
         srchbtn1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -159,16 +165,57 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //위젯 업데이트
-        update_widget.setOnClickListener(new View.OnClickListener(){
+        getWebsite();
+        //텍스트 버튼(크롤링)
+        main_txt = findViewById(R.id.main_txt);
+        main_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Widget.class);
-                intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-                MainActivity.this.sendBroadcast(intent);
+                getWebsite();
             }
         });
     }
+
+    //크롤링 연습
+    private void getWebsite() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+
+                try{
+                    Document doc = Jsoup.connect("http://ncov.mohw.go.kr/").get();
+                    String title = doc.title();
+                    Elements links = doc.select("div.datalist ul li");
+
+                    builder.append(title).append("\n");
+
+                    int i=0;
+                    for(Element link : links){
+                        String temp = link.select("span.data").text();
+                        infected[i++] = Integer.parseInt(temp);
+                        builder.append("\n").append(temp);
+                    }
+
+
+                }catch (IOException e){
+                    builder.append("Error : ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        main_txt.setText(Html.fromHtml("총 감염자 수<br /> " + "<font color=\"#000000\">" + Integer.toString(infected[0] + infected[1]) + "</font>"+ "명" + "<br />국내 감염자 수<br />"+"<font color=\"#9999FF\">" + Integer.toString(infected[0]) + "</font> 명"
+                        + "<br />해외 감염자 수<br /><font color=\"#FF9999\">" + Integer.toString(infected[1]) +"</font> 명"));
+                        /*Html.fromHtml("<font size='40' color=\"#9999FF\">" + Integer.toString(sharedPreferences.getInt("i_k", -1)) + "</font>" + " 명 "
+                                + "<font size='40' color=\"#FF9999\">" + Integer.toString(sharedPreferences.getInt("i_f", -1)) +"</font>" + " 명")*/
+                    }
+                });
+            }
+        }).start();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -227,8 +274,16 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("e3",Engine3);
         editor.putString("e4",Engine4);
 
+        //감염자 수 저장
+        editor.putInt("i_k",infected[0]);
+        editor.putInt("i_f",infected[1]);
 
         editor.commit();
+
+        //최소화했을때 자동 업데이트
+        Intent intent = new Intent(MainActivity.this, Widget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        MainActivity.this.sendBroadcast(intent);
     }
     // 앱 종료 후에 설정 값을 pref에 저장
     @Override
@@ -256,8 +311,16 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("e3",Engine3);
         editor.putString("e4",Engine4);
 
+        //감염자 수 저장
+        editor.putInt("i_k",infected[0]);
+        editor.putInt("i_f",infected[1]);
 
         editor.commit();
+
+        //종료했을때 자동 업데이트
+        Intent intent = new Intent(MainActivity.this, Widget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        MainActivity.this.sendBroadcast(intent);
     }
 
     @Override
--- app/src/main/java/com/example/exercise/Widget.java
+++ app/src/main/java/com/example/exercise/Widget.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.text.Html;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
@@ -20,10 +21,6 @@ import java.util.logging.LogRecord;
  */
 public class Widget extends AppWidgetProvider {
     private final String ACTION_RE = "ButtonClick";
-    private final String ACTION_BTN1 = "ButtonClick1";
-    private final String ACTION_BTN2 = "ButtonClick2";
-    private final String ACTION_BTN3 = "ButtonClick3";
-    private final String ACTION_BTN4 = "ButtonClick4";
     private static String temp_word1;
     private static String temp_engine1;
     private static String temp_word2;
@@ -32,10 +29,8 @@ public class Widget extends AppWidgetProvider {
     private static String temp_engine3;
     private static String temp_word4;
     private static String temp_engine4;
+    private static int[] infected = new int[2];
 
-    private final long FINISH_INTERVAL_TIME = 1000;
-
-    private static String uri1;
 
 
     @Override
@@ -48,12 +43,20 @@ public class Widget extends AppWidgetProvider {
             remoteViews.setTextViewText(R.id.wbtn2, sharedPreferences.getString("b2","실패"));
             remoteViews.setTextViewText(R.id.wbtn3, sharedPreferences.getString("b3","실패"));
             remoteViews.setTextViewText(R.id.wbtn4, sharedPreferences.getString("b4","실패"));
+            remoteViews.setTextViewText(R.id.txt, Html.fromHtml("<font color=\"#9999FF\">" + Integer.toString(sharedPreferences.getInt("i_k", -1)) + "</font>" + " 명 "
+                    + "<font color=\"#FF9999\">" + Integer.toString(sharedPreferences.getInt("i_f", -1)) +"</font>" + " 명"))
+            ;
+
+            Intent intent_txt = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent_txt = PendingIntent.getActivity(context, 0, intent_txt, 0);
+            remoteViews.setOnClickPendingIntent(R.id.txt, pendingIntent_txt);
 
             //버튼 새로고침
-            Intent intent_re = new Intent(context, Widget.class).setAction(appWidgetManager.ACTION_APPWIDGET_UPDATE);
+            Intent intent_re = new Intent(context, Widget.class).setAction(ACTION_RE);
+            //Intent intent_re = new Intent(context, Widget.class).setAction(appWidgetManager.ACTION_APPWIDGET_UPDATE);
             PendingIntent pendingIntent_re = PendingIntent.getBroadcast(context,0, intent_re, 0);
             remoteViews.setOnClickPendingIntent(R.id.refresh, pendingIntent_re);
-            
+
             //버튼1
             temp_word1 = sharedPreferences.getString("b1","tw1 불러오기 실패");
             temp_engine1 = sharedPreferences.getString("e1","https://m.google.com/search.google?q=");
@@ -87,18 +90,30 @@ public class Widget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
         ComponentName testWidget = new ComponentName(context.getPackageName(), Widget.class.getName());
 
         int[] widgetIds = appWidgetManager.getAppWidgetIds(testWidget);
 
+        //주기적 또는 어플에서 새로고침 눌렀을때
         if(intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)){
+            context.getSharedPreferences("widget", Context.MODE_PRIVATE).edit().putInt("click", 0).commit();
+            if(widgetIds != null && widgetIds.length >0) {
+                this.onUpdate(context, AppWidgetManager.getInstance(context), widgetIds);
+                //Toast.makeText(context, "새로고침 완료", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        //위젯에서 새로고침 버튼 눌렀을때, 메세지 출력
+        if(intent.getAction().equals(ACTION_RE)){
+            context.getSharedPreferences("widget", Context.MODE_PRIVATE).edit().putInt("click", 0).commit();
             if(widgetIds != null && widgetIds.length >0) {
                 this.onUpdate(context, AppWidgetManager.getInstance(context), widgetIds);
                 Toast.makeText(context, "새로고침 완료", Toast.LENGTH_SHORT).show();
             }
         }
+
+
     }
 
     @Override
